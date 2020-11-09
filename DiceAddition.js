607850deba789225031fcfd1df87133ef10e143e
Sanjana Jain <sanjana.jain04@gmail.com>
2020-11-10 02:06:23 +0530

    Adding two random numbers on a dice

--- DiceAddition.js
+++ DiceAddition.js
@@ -1,2 +1,6 @@
-let num = Math.floor(Math.random()*10) % 6 + 1;
-console.log("Dice Number Obtained : " + num);
+let num1 = Math.floor(Math.random()*10) % 6 + 1;
+let num2 = Math.floor(Math.random()*10) % 6 + 1;
+
+console.log("Number 1: "+num1);
+console.log("Numebr 2: "+num2);
+console.log("Sum of the two numbers: " + (num1+num2));
